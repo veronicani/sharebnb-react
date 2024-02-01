c247350be3f091c6e65798d4795e93c8cc96e451
Veronica Ni <vni1294@gmail.com>
2024-02-01 11:07:39 -0500

    Added bootstrap columning, changed checkbox classes to apply bs style, changed description to textarea type

--- src/AddPropertyForm.js
+++ src/AddPropertyForm.js
@@ -1,3 +1,4 @@
+import "./AddPropertyForm.css";
 import { useState } from "react";
 
 /** Form to add property.
@@ -27,7 +28,6 @@ function AddPropertyForm({ addProperty }) {
   function handleChange(evt) {
 
   }
-
   /** */
   function handleCheckBoxChange(evt) {
 
@@ -40,79 +40,84 @@ function AddPropertyForm({ addProperty }) {
   }
 
   return (
-    <div className="AddPropertyForm col-md-6 col-lg-4 offset-md-3 offset-lg-4">
-      <h3>Add your backyard or pool!</h3>
-      <div className="card">
-        <div className="card-body">
-          <form method="POST" enctype="multipart/form-data">
-            <div className="mb-3">
-              <label className="form-label">Property Name</label>
-              <input
-                name="name"
-                className="form-control"
-                value={formData.name}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Address</label>
-              <input
-                name="address"
-                className="form-control"
-                value={formData.address}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Description</label>
-              <input
-                name="description"
-                className="form-control"
-                value={formData.description}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Price</label>
-              <input
-                name="price"
-                className="form-control"
-                value={formData.price}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Backyard</label>
-              <input
-                type="checkbox"
-                name="backyard"
-                className="form-control"
-                value={formData.backyard}
-                onChange={handleCheckBoxChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Pool</label>
-              <input
-                type="checkbox"
-                name="pool"
-                className="form-control"
-                value={formData.pool}
-                onChange={handleCheckBoxChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Image</label>
-              <input
-                type="file"
-                name="image"
-                className="form-control"
-                value={formData.image}
-                onChange={handleChange}
-              />
-            </div>
+    <div className="AddPropertyForm">
+      <div className="container col-md-6 col-lg-4 offset-md-3 offset-lg-4">
+        <h3>Add your backyard or pool!</h3>
+        <div className="card">
+          <div className="card-body">
+            <form method="POST" enctype="multipart/form-data">
+              <div className="mb-3">
+                <label className="form-label">Property Name</label>
+                <input
+                  name="name"
+                  className="form-control"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Address</label>
+                <input
+                  name="address"
+                  className="form-control"
+                  value={formData.address}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Description</label>
+                <textarea
+                  name="description"
+                  className="form-control"
+                  value={formData.description}
+                  onChange={handleChange}
+                  rows="3"
+                ></textarea>
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Price</label>
+                <input
+                  type="number"
+                  name="price"
+                  className="form-control"
+                  value={formData.price}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="checkbox"
+                  name="backyard"
+                  className="form-check-input"
+                  value={formData.backyard}
+                  onChange={handleCheckBoxChange}
+                />
+                <label className="form-check-label">
+                  Backyard
+                </label>
+              </div>
+              <div className="mb-3">
+                <input
+                  type="checkbox"
+                  name="pool"
+                  className="form-check-input"
+                  value={formData.pool}
+                  onChange={handleCheckBoxChange}
+                />
+                <label className="form-check-label"> Pool</label>
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Image</label>
+                <input
+                  type="file"
+                  name="image"
+                  className="form-control"
+                  value={formData.image}
+                  onChange={handleChange}
+                />
+              </div>
 
-            {/* {formErrors.length
+              {/* {formErrors.length
               ? <Alert type="danger" messages={formErrors} />
               : null}
 
@@ -121,13 +126,13 @@ function AddPropertyForm({ addProperty }) {
               <Alert type="success" messages={["Updated successfully."]} />
               : null} */}
 
-            <div className="d-grid">
-              <button className="btn btn-primary" onClick={handleSubmit}>
-                Save Changes
-              </button>
-            </div>
-
-          </form>
+              <div className="d-grid">
+                <button className="btn btn-primary" onClick={handleSubmit}>
+                  Add Property
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
