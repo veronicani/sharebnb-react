9b9c07cd4a964e27f58c77ea3432092348963f8c
Veronica Ni <vni1294@gmail.com>
2024-02-01 19:41:17 -0500

    Implemented getPropertiesOnMount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import ShareBnB from './api';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RoutesList from './RoutesList';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -21,13 +21,26 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [properties, setProperties] = useState([]);
 
+  /** getProperties: Makes a request to API to get all properties. */
+
+  useEffect(function getPropertiesOnMount() {
+    async function fetchProperties() {
+      const { properties } = await ShareBnB.getProperties(); 
+      console.log('properties=', properties);
+      setProperties(properties);
+      setIsLoading(false);
+    }
+    fetchProperties();
+  }, []);  
+
   /** addProperty: Makes a request to API to add a new property. */
-  
+
   async function addProperty(formData, file){
     const resp = await ShareBnB.addProperty(formData, file);
     console.log("addProperty resp=", resp);
   }
 
+  if (isLoading === true) return <p>Loading...</p>;
 
   return (
     <div className="App">
