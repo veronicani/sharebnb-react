4ee539ba3fc8973a6c9ba80bb670a0f4adfd8684
Veronica Ni <vni1294@gmail.com>
2024-03-09 19:23:24 -0500

    Updated searchbar to be centered in lg screens

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -35,7 +35,7 @@ function SearchForm({ search }) {
   return (
     <div className="SearchForm mb-4">
       <form onSubmit={handleSubmit}>
-        <div className="row justify-content-center justify-content-lg-start gx-0">
+        <div className="row justify-content-center">
           <div className="col-8">
             <input
               className="form-control form-control-lg"
