9c826c1d9f25e56f68695fa89559a661b3ebf600
Veronica Ni <vni1294@gmail.com>
2026-02-01 21:37:23 -0500

    Add tests for navbar

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,9 +1,54 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './Navbar';
 
+// Mock the SVG logo
+jest.mock('../../images/sharebnb-logo-text-color.svg', () => ({
+  ReactComponent: () => <div data-testid="logo">ShareBnB Logo</div>
+}));
+
 describe('Navbar', () => {
-  it('renders without crashing', () => {
-    // TODO: Add tests
+  const mockSearch = jest.fn();
+
+  afterEach(() => {
+    mockSearch.mockClear();
+  });
+
+  function renderNavbar() {
+    return render(
+      <BrowserRouter>
+        <Navbar search={mockSearch} />
+      </BrowserRouter>
+    );
+  }
+
+  describe('Navigation Links', () => {
+    test('renders Add Property link', () => {
+      renderNavbar();
+
+      const addPropertyLink = screen.getByRole('link', { name: /add property/i });
+      expect(addPropertyLink).toBeInTheDocument();
+      expect(addPropertyLink).toHaveAttribute('href', '/add-property');
+    });
+
+    test('renders logo as link to home', () => {
+      renderNavbar();
+
+      const logoLink = screen.getByRole('link', { name: /sharebnb logo/i });
+      expect(logoLink).toBeInTheDocument();
+      expect(logoLink).toHaveAttribute('href', '/');
+    });
+  });
+
+  describe('Logo Interaction', () => {
+    test('calls search function when logo is clicked', () => {
+      renderNavbar();
+
+      const logoLink = screen.getByRole('link', { name: /sharebnb logo/i });
+      fireEvent.click(logoLink);
+
+      expect(mockSearch).toHaveBeenCalledTimes(1);
+      expect(mockSearch).toHaveBeenCalledWith();
+    });
   });
 });
