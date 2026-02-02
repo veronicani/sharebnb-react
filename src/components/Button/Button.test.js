a61ba14add34a5b8945572a88c80255b81f9bff0
Veronica Ni <vni1294@gmail.com>
2026-02-01 21:42:10 -0500

    Add button test

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,8 +1,20 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Button from './Button';
 
 describe('Button', () => {
-  it('renders without crashing', () => {
-    // TODO: Add tests
+  test('renders button with label text', () => {
+    render(<Button label="Click Me" handleClick={() => {}} />);
+
+    expect(screen.getByRole('button', { name: /click me/i })).toBeInTheDocument();
+  });
+
+  test('calls handleClick when button is clicked', () => {
+    const mockHandleClick = jest.fn();
+    render(<Button label="Submit" handleClick={mockHandleClick} />);
+
+    const button = screen.getByRole('button', { name: /submit/i });
+    fireEvent.click(button);
+
+    expect(mockHandleClick).toHaveBeenCalledTimes(1);
   });
 });
