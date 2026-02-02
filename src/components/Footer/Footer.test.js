26ce2e860e304d15349d89d305eabffcaf74c197
Veronica Ni <vni1294@gmail.com>
2026-02-01 21:42:58 -0500

    Add footer tests

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,8 +1,12 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Footer from './Footer';
 
 describe('Footer', () => {
-  it('renders without crashing', () => {
-    // TODO: Add tests
+  test('renders GitHub link with correct href', () => {
+    render(<Footer />);
+
+    const githubLink = screen.getByRole('link');
+    expect(githubLink).toBeInTheDocument();
+    expect(githubLink).toHaveAttribute('href', 'https://github.com/veronicani/sharebnb-react');
   });
 });
