8c142bf160a71416a2e48fb321a31891c514bff6
Veronica Ni <vni1294@gmail.com>
2026-02-01 21:57:10 -0500

    Loader test

--- src/components/Loader/Loader.test.js
+++ src/components/Loader/Loader.test.js
@@ -1,8 +1,10 @@
-import render from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Loader from './Loader';
 
 describe('Loader', () => {
-  test('renders without crashing', () => {
+  test('renders loading message', () => {
     render(<Loader />);
+
+    expect(screen.getByText(/server waking up\.\.\./i)).toBeInTheDocument();
   });
 });
